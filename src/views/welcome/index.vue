91c54b28b05a52ecd8e4de4a8282fd7ec30ac209
Christine-Only <tsy17321289030@163.com>
2022-10-15 22:02:47 +0800

    feat: init vue template

---
+++ src/views/welcome/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="welcome-container">
+    欢迎来到测试平台
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Welcome'
+}
+</script>
+
+<style lang="scss" scoped>
+.welcome-container {
+  padding: 24px;
+}
+</style>
